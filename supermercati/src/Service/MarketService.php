ccd3ae5aeacd51844e23196f4056872894ff79c1
SimoneP4nada <simone.panada7498@gmail.com>
2023-12-05 11:51:51 +0100

    .

--- supermercati/src/Service/MarketService.php
+++ supermercati/src/Service/MarketService.php
@@ -21,7 +21,7 @@ class MarketService
         $connection = $this->marketService->openConnection("supermercati");
 
         // Esegue la query per selezionare tutti i market utilizzando il servizio FunzioniDB
-        $arrayMarkets = $this->marketService->selectMarket($connection, false);
+        $arrayMarkets = $this->marketService->selectMarket($connection, false, $id);
         
         // Parametri da restituire, contenenti l'array di market
         $params = [
