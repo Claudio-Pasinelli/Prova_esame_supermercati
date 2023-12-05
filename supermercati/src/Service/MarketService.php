c9407843f7e4b60466cc0db338f14e30936ec2e3
SimoneP4nada <simone.panada7498@gmail.com>
2023-12-05 10:31:33 +0100

    Homepage settled

--- supermercati/src/Service/MarketService.php
+++ supermercati/src/Service/MarketService.php
@@ -18,7 +18,7 @@ class MarketService
     public function getData()
     {
         // Apre la connessione al database utilizzando il servizio FunzioniDB
-        $connection = $this->marketService->openConnection("NOEMDB");
+        $connection = $this->marketService->openConnection("NOMEDB");
 
         // Esegue la query per selezionare tutti i libri utilizzando il servizio FunzioniDB
         $arrayMarkets = $this->marketService->selectMarket($connection, false);
