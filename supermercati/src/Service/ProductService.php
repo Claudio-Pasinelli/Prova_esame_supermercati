485f7defea20825dce70c33bbf20fc9293ef4fdd
SimoneP4nada <simone.panada7498@gmail.com>
2023-12-05 10:53:20 +0100

    service per prenotazioni e prodotti

---
+++ supermercati/src/Service/ProductService.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Service;
+
+use App\Entity\Product;
+use App\FunzioniDB\FunzioniDB;
+
+class ProductService
+{
+    private $productService;
+
+    // Costruttore che accetta un'istanza di FunzioniDB come dipendenza
+    public function __construct(FunzioniDB $funzioniDBService)
+    {
+        $this->productService = $funzioniDBService;
+    }
+
+    // Metodo che ottiene i dati per la visualizzazione dei prodotti
+    public function getData()
+    {
+        // Apre la connessione al database utilizzando il servizio FunzioniDB
+        $connection = $this->productService->openConnection("NOMEDB");
+
+        // Esegue la query per selezionare tutti i prodotti utilizzando il servizio FunzioniDB
+        $arrayProducts = $this->productService->selectMarket($connection, false);
+        
+        // Parametri da restituire, contenenti l'array di prodotti
+        $params = [
+            'arrayProducts' => $arrayProducts,
+        ];
+
+        return $params;
+    }
+}
+?>
